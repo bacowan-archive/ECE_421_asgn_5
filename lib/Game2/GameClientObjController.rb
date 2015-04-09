5906efe71a23546d88b272f88eee48f11c4abe30
Ryan <kusters@e5-05-20.ece.ualberta.ca>
2015-04-09 13:53:30 -0600

    haphazard start

---
+++ lib/Game2/GameClientObjController.rb
@@ -0,0 +1,75 @@
+require 'thread'
+
+class GameClientObjController
+	def initialize(server)
+		@done = false
+		@server = server
+		@serverProxy = @server.proxy('game')
+		@printMutex = Mutex.new
+		@gameName = nil
+		@notificationCount = 0
+	end
+
+
+	def put(gameName, column)
+		ret = @serverProxy.put(gameName,column.to_i)
+	end
+
+	def quit(gameName, userName)
+		ret @serverProxy.quit(gameName,userName)
+		@done = true
+	end
+
+	def save(gameName)
+		ret = @serverProxy.save(gameName)
+	end
+
+	def getStats()
+		ret = @serverProxy.getStats()
+	end
+
+	def connectToGame(gameName,userName)
+		ret = @serverProxy.connectToGame(gameName,userName)
+		if ret == true
+			@gameName = args[1]
+		end
+	end
+
+	def hostGame(gameName, userName, gameType, dims)
+		ret =  @serverProxy.hostGame(gameName,userName,gameType,[dims[0].to_i,dims[1].to_i])
+		if ret == true
+			@gameName = gameName
+		end
+	end
+
+	def loadGame(gameName, userName)
+		ret =  @serverProxy.loadGame(gameName,userName)
+	end
+	
+
+	def _notifications
+		Thread.new {
+			while !@done
+				if @gameName != nil
+					_getNotification
+				end
+                                sleep(1)
+			end
+		}
+	end
+
+	def _getNotification
+		temp = @server.call_async('game.getNotification', @gameName, @notificationCount )
+		@notificationCount += 1
+		ret = temp#Marshal.load( temp )
+		@printMutex.synchronize {
+			puts ret
+			begin
+				puts ''
+				print ret[1]
+				puts ''
+			rescue
+			end
+		}
+	end
+end
---
+++ lib/src/view/Host_Screen.rb
@@ -0,0 +1,23 @@
+require 'gtk2'
+
+class HostScreen
+
+def initialize(parentBoard)
+ @parent = parentBoard
+ Gtk.init
+ @builder = Gtk::Builder::new
+ @builder.add_from_file("./lib/src/view/Host_Screen.glade")
+ 
+ @builder.get_object("button1").signal_connect("clicked")
+{
+	setParentNames(@builder.get_object("entry1").text,@builder.get_object("entry2").text)
+	window.destroy
+}
+ @builder.get_object("button2").signal_connect("clicked"){window.destroy}
+
+end
+
+def setParentNames(gameName, userName)
+ @parent.setNames(gameName, userName)
+end
+
